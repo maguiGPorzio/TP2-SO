709dffc3a583757b17017776d2dc8ca05012d2f3
maguiGPorzio <mgonzalezporzio@itba.edu.ar>
2025-09-30 16:25:41 -0300

    Cree el mm

---
+++ Kernel/libraries/buddyMemoryManager.c
@@ -0,0 +1,8 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
+#ifdef USE_BUDDY
+
+#include "memoryManager.h"
+#include <stdint.h>
+#include <stddef.h>
+
