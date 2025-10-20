0d65458a3cde9d5f67cbd95624fa1b1011d823e5
Magali Gonzalez Porzio <mgonzalezporzio@itba.edu.ar>
2025-10-20 13:37:16 -0300

    modified mm.c

--- Kernel/memory/memoryManager.c
+++ Kernel/memory/memoryManager.c
@@ -1,8 +1,6 @@
 // This is a personal academic project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
 
-#ifndef USE_BUDDY
-
 #include "memoryManager.h"
 #include <string.h>
 #include <stdbool.h>
@@ -234,5 +232,3 @@ void initKernelMemoryManager(void) {
 MemoryManagerADT getKernelMemoryManager(void) {
     return kernelMemManager;
 }
-
-#endif 
