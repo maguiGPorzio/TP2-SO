2fcb5f80d16a4c1bceb103960d72759712419678
azugrelle <agrelle@itba.edu.ar>
2025-11-07 00:05:40 -0300

    refactor: eliminé el mutex de mvar pues es innecesario porque la capacidad del buffer es de 1

--- Userland/programs/mvar.c
+++ Userland/programs/mvar.c
@@ -2,7 +2,6 @@
 
 #define SEM_NAME_MAX 64
 #define SEM_PREFIX "mvar_"
-#define SEM_MUTEX_SUFFIX "mutex_"
 #define SEM_EMPTY_SUFFIX "empty_"
 #define SEM_FULL_SUFFIX  "full_"
 
@@ -10,11 +9,10 @@
 #define LETTER_POOL_SIZE (sizeof(LETTER_POOL) - 1)
 
 #define COLOR_COUNT 4
-#define MIN_SLEEP_MS 50
-#define SLEEP_JITTER_MS 250
+#define MIN_SLEEP_MS 10
+#define SLEEP_JITTER_MS 30
 
 static volatile char mvar_value = 0;
-static char sem_mutex_name[SEM_NAME_MAX];
 static char sem_empty_name[SEM_NAME_MAX];
 static char sem_full_name[SEM_NAME_MAX];
 
@@ -29,7 +27,6 @@ static int spawn_writer(int index);
 static int spawn_reader(int index);
 static int writer_process(int argc, char *argv[]);
 static int reader_process(int argc, char *argv[]);
-static int parse_positive(char *arg);
 
 int mvar_main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -37,8 +34,8 @@ int mvar_main(int argc, char *argv[]) {
         return ERROR;
     }
 
-    int num_writers = parse_positive(argv[0]);
-    int num_readers = parse_positive(argv[1]);
+    int num_writers = satoi(argv[0]);
+    int num_readers = satoi(argv[1]);
 
     if (num_writers <= 0 || num_readers <= 0) {
         print_err("mvar: los parámetros deben ser enteros positivos.\n");
@@ -71,14 +68,6 @@ int mvar_main(int argc, char *argv[]) {
     return OK;
 }
 
-static int parse_positive(char *arg) {
-    if (arg == NULL) {
-        return -1;
-    }
-    int value = (int)satoi(arg);
-    return value > 0 ? value : -1;
-}
-
 static void build_sem_name(char *target, const char *suffix, uint64_t pid) {
     char pid_buf[DECIMAL_BUFFER_SIZE];
     num_to_str(pid, pid_buf, 10);
@@ -104,16 +93,11 @@ static void build_sem_name(char *target, const char *suffix, uint64_t pid) {
 
 static void setup_sem_names(void) {
     uint64_t pid = sys_getpid();
-
-    build_sem_name(sem_mutex_name, SEM_MUTEX_SUFFIX, pid);
     build_sem_name(sem_empty_name, SEM_EMPTY_SUFFIX, pid);
     build_sem_name(sem_full_name, SEM_FULL_SUFFIX, pid);
 }
 
 static int attach_to_sync_objects(void) {
-    if (sys_sem_open(sem_mutex_name, 1) < 0) {
-        return ERROR;
-    }
     if (sys_sem_open(sem_empty_name, 1) < 0) {
         return ERROR;
     }
@@ -172,11 +156,8 @@ static int writer_process(int argc, char *argv[]) {
     while (1) {
         random_pause();
         sys_sem_wait(sem_empty_name);
-        sys_sem_wait(sem_mutex_name);
-
+        /* No mutex used: write directly to the single mvar */
         mvar_value = letter;
-
-        sys_sem_post(sem_mutex_name);
         sys_sem_post(sem_full_name);
     }
     return OK;
@@ -200,14 +181,12 @@ static int reader_process(int argc, char *argv[]) {
 
     while (1) {
         random_pause();
-
         sys_sem_wait(sem_full_name);
-        sys_sem_wait(sem_mutex_name);
 
+        /* No mutex used: read and clear mvar directly */
         char c = mvar_value;
         mvar_value = 0;
 
-        sys_sem_post(sem_mutex_name);
         sys_sem_post(sem_empty_name);
 
         sys_write(color, &c, 1);
