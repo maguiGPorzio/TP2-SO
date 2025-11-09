0d6a347af08955c65774f759596b2c0a0f3c7fa5
azugrelle <agrelle@itba.edu.ar>
2025-11-09 13:33:25 -0300

    pvs comment in folder Userland/shell

--- Userland/shell/shell.c
+++ Userland/shell/shell.c
@@ -1,3 +1,6 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
+
 #include "../include/shell.h"
 
 // Buffer para el comando actual y el anterior
