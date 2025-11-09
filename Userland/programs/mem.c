3885896553a1839161c78eca45a7703b810faa5f
azugrelle <agrelle@itba.edu.ar>
2025-11-09 13:32:30 -0300

    pvs comment in folder Toolchain and Userland/programs

--- Userland/programs/mem.c
+++ Userland/programs/mem.c
@@ -1,4 +1,5 @@
-#include "usrlib.h"
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
 
 #include "usrlib.h"
 
--- Userland/programs/mvar.c
+++ Userland/programs/mvar.c
@@ -1,3 +1,6 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
+
 #include "usrlib.h"
 
 #define MAX_SEM_NAME_LENGTH 64 // TODO: se comparte con Kernel
